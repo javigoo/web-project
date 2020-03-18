afcce5ed726a9f9d3b6e7e0dfbcf6a02f79734a8
javigoo <javigo_@outlook.com>
2020-03-18 21:41:49 +0100

    Removed Token Authentication (fail)

--- web_application/urls.py
+++ web_application/urls.py
@@ -14,10 +14,8 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
-from rest_framework.authtoken.views import obtain_auth_token
+from django.urls import path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('token-auth/', obtain_auth_token, name='token_auth'),
 ]

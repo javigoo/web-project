62310d5ba19fbb166085ebfb88f1fad160f5cfb3
javigoo <javigo_@outlook.com>
2020-03-19 13:03:21 +0100

    home view (sigue siendo un gato)

--- apps/spotify/views.py
+++ apps/spotify/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
 
 # Create your views here.
-def gato(request):
-    return render(request, 'gato.html', {})
+def home(request):
+    return render(request, 'home.html', {})

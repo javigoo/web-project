db54733c22072187cdf5b3062c53757e398c2cfd
javigoo <javigo_@outlook.com>
2020-03-19 14:11:35 +0100

    Added base.html template

--- web_application/settings.py
+++ web_application/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'web_application.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['web_application/templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [

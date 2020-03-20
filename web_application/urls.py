6d3eb5d4a1e26580e45a06184211337c655acf8c
jal04 <josemi.bnf@outlook.es>
2020-03-20 17:18:23 +0100

    auth app added

--- web_application/urls.py
+++ web_application/urls.py
@@ -20,6 +20,7 @@ from django.urls import path, include
 urlpatterns = [
     path('', include('social_django.urls', namespace='social')),
     path('logout/', auth_views.LogoutView.as_view(next_page='/'), name='logout'),
+    path('accounts/', include('django.contrib.auth.urls')),
     
     path('admin/', admin.site.urls),
     path('', include('apps.spotify.urls')),

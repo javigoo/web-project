920b9d96339c3bf6d1ae81d44b685fd5bfb756e4
jal04 <josemi.bnf@outlook.es>
2020-03-20 16:50:59 +0100

    auth2 añadido en settings, (main)urls.py, pero no funciona en home.html

--- web_application/urls.py
+++ web_application/urls.py
@@ -14,9 +14,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.contrib.auth import views as auth_views
 from django.urls import path, include
 
 urlpatterns = [
+    path('', include('social_django.urls', namespace='social')),
+    path('logout/', auth_views.LogoutView.as_view(next_page='/'), name='logout'),
+    
+    path('admin/', admin.site.urls),
     path('', include('apps.spotify.urls')),
-    path('admin/', admin.site.urls)
 ]

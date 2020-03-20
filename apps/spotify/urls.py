44d1a81722ff53c808aab0b30d472b0a8d2c9697
jal04 <josemi.bnf@outlook.es>
2020-03-20 01:05:41 +0100

    app_name on urls.py de spotify

--- apps/spotify/urls.py
+++ apps/spotify/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from . import views
 
+app_name = 'spotify'
 urlpatterns = [
     path('', views.home, name='home'),
     path('top/', views.topSongs, name='top tracks')
--- apps/spotify/views.py
+++ apps/spotify/views.py
@@ -9,7 +9,7 @@ def home(request):
 
 def topSongs(request):
     try:
-        lista = Song.objects.order_by('-views')[:10] #Top 10 Songs by views
+        lista = Song.objects.order_by('views')[:10] #Top 10 Songs by views
     except len(lista)==0:
         raise Http404("No tenemos canciones.")
     return render(request, 'spotify/topsongs.html', {'topsonglist':lista})

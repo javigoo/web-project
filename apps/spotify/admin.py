5cd0df4c68f1a1f3d606ec9ce0e40c61e0768a2a
javigoo <javierroiggregorio@gmail.com>
2020-03-21 20:49:33 +0100

    Añadido y modificación de Modelos

--- apps/spotify/admin.py
+++ apps/spotify/admin.py
@@ -4,4 +4,4 @@ from .models import *
 # Register your models here.
 admin.site.register(Artist)
 admin.site.register(Song)
-admin.site.register(Album)
+admin.site.register(Playlist)
--- apps/spotify/models.py
+++ apps/spotify/models.py
@@ -1,14 +1,32 @@
 from django.db import models
 
 # Create your models here.
-class Artist(models.Model):
-    name = models.CharField(max_length=200)
-    views = models.IntegerField(default=0)
 
 class Song(models.Model):
-    name = models.CharField(max_length=200)
-    date = models.DateTimeField('date published')
-    views = models.IntegerField(default=0)
+    name = models.CharField(max_length=200, null=True)
+    views = models.IntegerField(default=0, null=True)
+    date = models.DateTimeField('date published', null=True)
 
-class Album(models.Model):
-    name = models.CharField(max_length=200)
+    #song = models.ForeignKey(Song, null=True, on_delete= models.SET_NULL)
+
+    def __str__(self):
+        return self.name     # Esto nos permite ver el nombre desde Django Administration
+
+class Artist(models.Model):
+    name = models.CharField(max_length=200, null=True)
+    views = models.IntegerField(default=0, null=True)
+    information = models.CharField(default='No Artist Information', max_length=200, null=True)
+
+    song = models.ManyToManyField(Song)
+
+    def __str__(self):
+        return self.name
+
+class Playlist(models.Model):
+    name = models.CharField(max_length=200, null=True)
+    duration = models.IntegerField(default=0, null=True)
+
+    song = models.ManyToManyField(Song)
+
+    def __str__(self):
+        return self.name

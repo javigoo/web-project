5c414e00d3737a55d5baf08e0a50105d022623b8
jal04 <josemi.bnf@outlook.es>
2020-03-21 23:25:33 +0100

    Eliminados AOuth2 de setting.py y de urls.py

--- web_application/urls.py
+++ web_application/urls.py
@@ -17,8 +17,7 @@ from django.contrib import admin
 from django.contrib.auth import views as auth_views
 from django.urls import path, include
 
-urlpatterns = [
-    
+urlpatterns = [    
     path('admin/', admin.site.urls),
     path('', include('apps.spotify.urls')),
 ]

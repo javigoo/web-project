f2d350be79f8b608994dc1ce99efe8cd68296fee
javigoo <javierroiggregorio@gmail.com>
2020-03-22 15:02:12 +0100

    Fixed templates address

--- apps/spotify/views.py
+++ apps/spotify/views.py
@@ -5,11 +5,11 @@ from .models import Song
 
 # Create your views here.
 def home(request):
-    return render(request, 'spotify/home.html', {})
+    return render(request, 'home.html', {})
 
 def topSongs(request):
     try:
         lista = Song.objects.order_by('views')[:10] #Top 10 Songs by views
     except len(lista)==0:
         raise Http404("No tenemos canciones.")
-    return render(request, 'spotify/topsongs.html', {'topsonglist':lista})
+    return render(request, 'topsongs.html', {'topsonglist':lista})

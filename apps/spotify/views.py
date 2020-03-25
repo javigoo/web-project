621c1df4d5ff3a5131489fe46765ef46cd1d7b11
jal04 <josemi.bnf@outlook.es>
2020-03-25 12:28:28 +0100

    Añadido en navBar nombre de usuario con enlace a su página y boton de logout

--- apps/spotify/views.py
+++ apps/spotify/views.py
@@ -14,7 +14,7 @@ def top_songs(request):
     try:
         lista = Song.objects.order_by('views').reverse()[:10] #Top 10 Songs by views
     except len(lista)==0:
-        raise Http404("No tenemos canciones.")
+        raise Http404("Error, lista vacia.")
     return render(request, 'spotify/topsongs.html', {'topsonglist':lista})
 
 def profile(request):

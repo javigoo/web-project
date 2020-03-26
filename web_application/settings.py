0994d0327580af67fa361b216dac6ed4978df4c1
Gaby <gabygarcia211998@gmail.com>
2020-03-26 13:51:03 +0100

    Heroku push

--- web_application/settings.py
+++ web_application/settings.py
@@ -146,3 +146,17 @@ SOCIAL_AUTH_PIPELINE = (
     'social_core.pipeline.social_auth.load_extra_data',
     'social_core.pipeline.user.user_details',
 )
+#Update databse configuration with $DATABASE_URL.
+import dj_database_url
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = '/static/'
+
+#Extra places for collesctsatic to find static files
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR,'static'),
+)
+
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'

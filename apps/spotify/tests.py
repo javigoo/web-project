232aa1a3df4f58c15d20eba57aaead2e0242ef82
javigoo <javierroiggregorio@gmail.com>
2020-03-28 19:19:22 +0100

    Test comentado

--- apps/spotify/tests.py
+++ apps/spotify/tests.py
@@ -4,6 +4,9 @@ from apps.spotify.models import *
 # Create your tests here.
 
 class PlaylistTest(TestCase):
+    pass
+
+    """
     def setUp(self):
         User.objects.create(name='carlos')
         Playlist.objects.create(id='1234', name='pop', duration=124, user=User.objects.get(name='carlos'))
@@ -14,5 +17,6 @@ class PlaylistTest(TestCase):
         print(pop.user.name)
         print(carlos.name)
         self.assertEqual( pop.user.name, carlos.name)
+    """
 
 # Test travis (commit)

9befb9dadba2371d61afd2d470b703e9458e2af7
javigoo <javierroiggregorio@gmail.com>
2020-05-10 00:12:37 +0200

    Tests - add test_playlist()

--- apps/spotify/test.py
+++ apps/spotify/test.py
@@ -1,10 +1,17 @@
+
 from django.test import TestCase
+
+from .models import *
 
 
 class PlaylistTestCase(TestCase):
 
-    print("\n########### Start test.py ###########\n")
+    def setUp(self):
+        user = Spotify_User.objects.create(name="user")
+        Playlist.objects.create(id=1234567890, name="playlist", duration=10, user=user)
 
-    def test_assertTrue(self):
-        print("\n########### Start test_assertTrue ###########\n")
-        self.assertTrue(True)
+    def test_playlist(self):
+        """ Check if the user who created the playlist is correct """
+        playlist = Playlist.objects.get(id=1234567890)
+        user = Spotify_User.objects.get(name='user')
+        self.assertEqual(playlist.user.name, user.name)

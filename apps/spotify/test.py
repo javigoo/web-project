9d148bd6f57360b49e523e0721e3b20402f5c140
javigoo <javierroiggregorio@gmail.com>
2020-05-09 23:39:18 +0200

    Tests - Start working on test.py

---
+++ apps/spotify/test.py
@@ -0,0 +1,10 @@
+from django.test import TestCase
+
+
+class PlaylistTestCase(TestCase):
+
+    print("\n########### Start test.py ###########\n")
+
+    def test_assertTrue(self):
+        print("\n########### Start test_assertTrue ###########\n")
+        self.assertTrue(True)

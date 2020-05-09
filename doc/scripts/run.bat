0e8a98cd747c0afa0163926b2a242cac253e5f8b
javigoo <javierroiggregorio@gmail.com>
2020-05-09 23:47:04 +0200

    Tests - Fix db issues

--- doc/scripts/run.bat
+++ doc/scripts/run.bat
@@ -2,4 +2,5 @@
 cd ..\..\
 python -m venv venv && venv\Scripts\activate && pip3 install -r requirements.txt
 python manage.py makemigrations && python manage.py migrate && python manage.py runserver
+python manage.py test apps.spotify.test
 exit
